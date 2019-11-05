d534be4ed7b728034e50144f8bfa70498d45952a
Brenii <nyakoabrenda@gmail.com>
2019-11-05 21:02:00 +0300

    module 3 fourth update

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -3,9 +3,10 @@ import {Http,Response} from '@angular/http';
 import 'rxjs/add/operator/map';
 @Injectable()
 export class ProductService {
-private _albumUrl='../assets/album.json';
-  constructor(private _http:Http) {}//good practice for injecting an istance of a service 
-getAlbum(id: number) {
+ private _albumUrl='../assets/album.json';
+ constructor(private _http:Http) {}//good practice for injecting an istance of a service 
+ 
+ getAlbum(id: number) {
   return this._http.get(this._albumUrl).map((response)=>{response.json();})
-} 
+ } 
 }
