fa02405722650cd23f0161be9b68ad78c63d9de7
Brenii <nyakoabrenda@gmail.com>
2019-11-05 22:03:48 +0300

    module 3 revision

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -6,7 +6,7 @@ export class ProductService {
  private _albumUrl='../assets/album.json';
  constructor(private _http:Http) {}//good practice for injecting an istance of a service 
  
- getAlbum(id) {
-  return this._http.get(this._albumUrl).map((response)=>{response.json();})
+ getAlbum(id: number) {
+  return this._http.get(this._albumUrl).map((response)=>{response.json()})
  } 
 }
