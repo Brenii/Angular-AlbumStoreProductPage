944ae0666ea9889288a0b892c9a403979810c63d
Brenii <nyakoabrenda@gmail.com>
2019-11-08 14:14:28 +0300

    module 6 first submission

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,14 +7,16 @@ import { AppComponent } from './app.component';
 import { ProductPageComponent } from './product-page/product-page.component';
 import { ProductDescriptionComponent } from './product-description/product-description.component';
 import {ProductService}  from './product.service';
-import { ProductTracklistingComponent } from './product-tracklisting/product-tracklisting.component';//importing ProducService class in app module.
+import { ProductTracklistingComponent } from './product-tracklisting/product-tracklisting.component';
+import { ProductListComponent } from './product-list/product-list.component';//importing ProducService class in app module.
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductPageComponent,
     ProductDescriptionComponent,
-    ProductTracklistingComponent
+    ProductTracklistingComponent,
+    ProductListComponent
   ],
   imports: [
     BrowserModule,
